959b00848f8cb4cb4a717ac15db655b9345442e4
Hugo Fonseca <hugo.fonseca@beubi.com>
2015-03-09 18:47:35 +0000

    Added get

--- src/Application/Command/RunSeleniumCommand.php
+++ src/Application/Command/RunSeleniumCommand.php
@@ -14,6 +14,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Helper\ProgressBar;
 
 class RunSeleniumCommand extends Command
 {
@@ -30,14 +31,26 @@ class RunSeleniumCommand extends Command
         ->addArgument(
             'action',
             InputArgument::OPTIONAL,
-            'start|stop',
+            'start|stop|get',
             'start'
         )
         ->addOption(
             'firefox-profile',
-            'fp',
+            'p',
             InputOption::VALUE_REQUIRED,
             'Give a custom firefox profile location'
+        )
+        ->addOption(
+            'selenium-version',
+            's',
+            InputOption::VALUE_REQUIRED,
+            '(get only) Set a custom selenium version'
+        )
+        ->addOption(
+            'selenium-destination',
+            'd',
+            InputOption::VALUE_REQUIRED,
+            '(get only) Set a custom selenium destination'
         )
         ->setDescription('This will start/stop Selenium2 server.');
     }
@@ -58,8 +71,49 @@ class RunSeleniumCommand extends Command
                 $cmd = $root.'/src/Scripts/selenium.sh -a stop '.$verboseCmd;
                 $this->runCmdToStdOut($cmd);
                 break;
+            case 'get':
+                $version = $input->getOption('selenium-version') ?: '2.44';
+                $destination = $input->getOption('selenium-destination') ?: __DIR__ . '/../../../bin';
+                $this->updateSelenium($input, $output, $version, $destination);
+                break;
             default:
                 throw new \RuntimeException('Invalid Argument');
+        }
+    }
+
+    private function downloadFile(OutputInterface $output, $url, $outputFile)
+    {
+        $progress = new ProgressBar($output);
+        $ctx = stream_context_create(array(), array('notification' => function ($notification_code, $severity, $message, $message_code, $bytes_transferred, $bytes_max) use ($output, $progress) {
+            switch ($notification_code) {
+                case STREAM_NOTIFY_FILE_SIZE_IS:
+                    $progress->start($bytes_max);
+                    break;
+                case STREAM_NOTIFY_PROGRESS:
+                    $progress->setCurrent($bytes_transferred);
+                    break;
+            }
+        }));
+        $file = file_get_contents($url, false, $ctx);
+        file_put_contents($outputFile, $file);
+        $progress->finish();
+    }
+
+    private function updateSelenium(InputInterface $input, OutputInterface $output, $version, $destination)
+    {
+        if (!is_writable(dirname($destination))) {
+            throw new \RuntimeException('Not enought permissions. Try with sudo.');
+        }
+        if (!is_dir($destination)) {
+            mkdir($destination, 0777, true);
+        }
+        $outputFile = $destination.'/selenium-server-standalone.jar';
+        $url = 'http://selenium-release.storage.googleapis.com/' . $version . '/selenium-server-standalone-' . $version . '.0.jar';
+        $this->downloadFile($output, $url, $outputFile);
+        $output->writeln('Done');
+
+        if (!file_exists($outputFile)) {
+            throw new \LogicException('Something wrong happent: ' . $outputFile);
         }
     }
 
